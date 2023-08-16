81a4788cf7e3806ce8c1b929a44dfef487bd4cf0
nyalipio <141049947+nyalipio@users.noreply.github.com>
2023-08-16 14:58:00 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,83 @@
-<?php
-require "//Conn.php";
-require ".//produto.php";
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kit Festa</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Kit Festa 10 pessoas.</h1>
+        <nav>
+            <ul>
+                <li><a href="#">1,5kg bolo (creme de chocolate com bombons, floresta negra, Mousse de Maracujá ou creme com Morango</a></li>
+                <li><a href="#">30 docinhos (brigadeiro e beijinho)a></li>
+                <li><a href="#">50 salgados (coxinha, bolinha de queijo, kibe ou esfiha carne)</a></li>
+                <li><a href="#">2 litros refrigerante</a></li>
+                <li><a href="#">Descartáveis (pratinhos, guardanapos, garfinhos e 1 vela)</a></li>
+            </ul>
+        </nav>
 
-$novo = new Product();
-$res =  $novo->listarprodutodos();
-foreach($res as $prod) {
-    extract($prod);
-?>
+         <h1>Kit Festa 20 pessoas.</h1>
+        <nav>
+            <ul>
+                <li><a href="#">2kg bolo (creme de chocolate com bombons, floresta negra, Mousse de Maracujá ou creme com Morango</a></li>
+                <li><a href="#">150 docinhos (brigadeiro e beijinho)a></li>
+                <li><a href="#">100 salgados (coxinha, bolinha de queijo, kibe ou esfiha carne)</a></li>
+                <li><a href="#">4 litros refrigerante</a></li>
+                <li><a href="#">Descartáveis (pratinhos, guardanapos, garfinhos e 1 vela)</a></li>
+            </ul>
+        </nav>
 
-<div class="card-deck">
-<div style="margin: 4px;" class= "card">
-<img class="cadr-img-top" src= "<?php-echo"
+         <h1>Kit Festa 30 pessoas.</h1>
+        <nav>
+            <ul>
+                <li><a href="#">3kg bolo (creme de chocolate com bombons, floresta negra, Mousse de Maracujá ou creme com Morango</a></li>
+                <li><a href="#">150 docinhos (brigadeiro e beijinho)a></li>
+                <li><a href="#">100 salgados (coxinha, bolinha de queijo, kibe ou esfiha carne)</a></li>
+                <li><a href="#">6 litros refrigerante</a></li>
+                <li><a href="#">Descartáveis (pratinhos, guardanapos, garfinhos e 1 vela)</a></li>
+            </ul>
+        </nav>
+
+         <h1>Kit Festa 50 pessoas.</h1>
+        <nav>
+            <ul>
+                <li><a href="#">4kg bolo (creme de chocolate com bombons, floresta negra, Mousse de Maracujá ou creme com Morango</a></li>
+                <li><a href="#">250 docinhos (brigadeiro e beijinho)a></li>
+                <li><a href="#">200 salgados (coxinha, bolinha de queijo, kibe ou esfiha carne)</a></li>
+                <li><a href="#">8 litros refrigerante</a></li>
+                <li><a href="#">Descartáveis (pratinhos, guardanapos, garfinhos e 1 vela)</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <section class="hero">
+        <h2>Seu Kit Festa Completo</h2>
+        <p>Faça sua festa com nossos incríveis kits festa! Pela metade do preço</p>
+        <a href="#" class="cta-button">Ver Produtos</a>
+    </section>
+    
+    <section class="products">
+        <h2>Produtos em Destaque</h2>
+        <div class="product">
+            <img src="produto1.jpg" alt="Kit Festa 1">
+            <h3>Kit Festa Premium</h3>
+            <p>Descrição do kit festa premium. Inclui balões, decorações e muito mais.</p>
+            <a href="#" class="product-button">Ver Detalhes</a>
+        </div>
+        <div class="product">
+            <img src="produto2.jpg" alt="Kit Festa 2">
+            <h3>Kit Festa Básico</h3>
+            <p>Descrição do kit festa básico. Perfeito para festas pequenas e íntimas.</p>
+            <a href="#" class="product-button">Ver Detalhes</a>
+        </div>
+        <!-- Adicione mais produtos aqui -->
+    </section>
+    
+    <footer>
+        <p>&copy; 2023 Kit Festa Inc. Todos os direitos reservados.</p>
+    </footer>
+</body>
+</html>
